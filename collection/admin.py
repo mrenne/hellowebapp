21fd6602a9b7cc490e706513ff2da98291028d15
Mike <mike.renne@gmail.com>
2017-02-24 14:37:08 -0500

    End of Chapter 6

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from collection.models import Tool
+
+class ToolAdmin(admin.ModelAdmin):
+   model = Tool
+   list_display = ('name', 'description',)
+   prepopulated_fields = {'slug': ('name',)}
+
+
 # Register your models here.
+admin.site.register(Tool, ToolAdmin)
