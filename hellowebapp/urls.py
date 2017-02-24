ff545d80da49b4e7dee9d0d3c5f4439cf9a54f22
Mike <mike.renne@gmail.com>
2017-02-23 22:14:25 -0500

    End of Chapter 5

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^about/$',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'),
+	name='contact'),
+
+
 ]
